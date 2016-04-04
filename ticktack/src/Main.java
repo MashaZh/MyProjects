ce966f9fa959b6401b9f8215712b0521f9ec85d0
Masha <Mashazh@yandex.ru>
2016-04-04 21:16:23 +0300

    пробуем

---
+++ ticktack/src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Masha on 07.03.2016.
+ */
+public class Main {
+    public static Ticktack ticktack;
+
+    public static void main(String[] args) {
+
+        ticktack = new Ticktack();
+        ticktack.game();
+    }
+}
